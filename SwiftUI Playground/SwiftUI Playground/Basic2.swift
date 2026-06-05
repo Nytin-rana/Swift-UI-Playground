8e5df5aec217a6f3e57e68119dcd49d1d5656447
nytin <nytinr@gmail.com>
2026-06-05 14:46:31 +0530

    basic 2 completeed

--- SwiftUI Playground/SwiftUI Playground/Basic2.swift
+++ SwiftUI Playground/SwiftUI Playground/Basic2.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct Basic2: View {
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading, spacing: 20){
+            VStack(alignment: .leading, spacing: 10){
                 Text("Text Editor :").fontWeight(.bold).foregroundColor(.gray)
                 TextEditor(text: .constant("Hey there, this is a text editor! "))
                     .frame(height: 100)
@@ -22,6 +22,22 @@ struct Basic2: View {
                     .padding(.leading,40)
                     
                 Text("Shapes :").fontWeight(.bold).foregroundColor(.gray)
+                HStack{
+                    
+                    Rectangle().foregroundColor(.red).frame(width: 100, height: 100)
+                    
+                    Capsule().foregroundColor(.green).frame(width: 100, height: 100)
+                    
+                    RoundedRectangle(cornerRadius:10).foregroundColor(.blue).frame(width: 100, height: 100)
+                }
+                
+                HStack{
+                    
+                    Circle().foregroundColor(.orange).frame(width: 100, height: 100)
+                    
+                    Ellipse().foregroundColor(.teal).frame(width: 200, height: 100)
+                }
+                
                 
                 
                 Spacer()
